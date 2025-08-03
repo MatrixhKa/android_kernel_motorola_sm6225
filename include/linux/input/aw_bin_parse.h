3f147e5ce79aa4396e7735283fbdb04ca05ca28a
angelomds42 <angelomds42@gmail.com>
2025-08-03 11:03:34 -0400

    aw9610x: fix compilation error
    Make some functions static and inline others

--- include/linux/input/aw_bin_parse.h
+++ include/linux/input/aw_bin_parse.h
@@ -1,10 +1,11 @@
 #ifndef __AW_BIN_PARSE_H__
 #define __AW_BIN_PARSE_H__
 
-#define NULL    ((void *)0)
-#define GET_32_DATA(w, x, y, z) ((unsigned int)(((w) << 24) | ((x) << 16) | ((y) << 8) | (z)))
-#define BIN_NUM_MAX   100
-#define HEADER_LEN    60
+#define NULL ((void *)0)
+#define GET_32_DATA(w, x, y, z)                                                \
+	((unsigned int)(((w) << 24) | ((x) << 16) | ((y) << 8) | (z)))
+#define BIN_NUM_MAX 100
+#define HEADER_LEN 60
 /*********************************************************
  *
  * header information
@@ -24,14 +25,15 @@ enum data_type_enum {
 };
 
 enum data_version_enum {
-	DATA_VERSION_V1 = 0X00000001,	/*default little edian */
+	DATA_VERSION_V1 = 0X00000001, /*default little edian */
 	DATA_VERSION_MAX,
 };
 
 struct bin_header_info {
 	unsigned int header_len; /* Frame header length */
 	unsigned int check_sum; /* Frame header information-Checksum */
-	unsigned int header_ver; /* Frame header information-Frame header version */
+	unsigned int
+		header_ver; /* Frame header information-Frame header version */
 	unsigned int bin_data_type; /* Frame header information-Data type */
 	unsigned int bin_data_ver; /* Frame header information-Data version */
 	unsigned int bin_data_len; /* Frame header information-Data length */
@@ -40,13 +42,18 @@ struct bin_header_info {
 	unsigned int reg_byte_len; /* Frame header information-reg byte len */
 	unsigned int data_byte_len; /* Frame header information-data byte len */
 	unsigned int device_addr; /* Frame header information-device addr */
-	unsigned int valid_data_len; /* Length of valid data obtained after parsing */
-	unsigned int valid_data_addr; /* The offset address of the valid data obtained after parsing relative to info */
+	unsigned int
+		valid_data_len; /* Length of valid data obtained after parsing */
+	unsigned int
+		valid_data_addr; /* The offset address of the valid data obtained after parsing relative to info */
 
 	unsigned int reg_num; /* The number of registers obtained after parsing */
-	unsigned int reg_data_byte_len; /* The byte length of the register obtained after parsing */
-	unsigned int download_addr; /* The starting address or download address obtained after parsing */
-	unsigned int app_version; /* The software version number obtained after parsing */
+	unsigned int
+		reg_data_byte_len; /* The byte length of the register obtained after parsing */
+	unsigned int
+		download_addr; /* The starting address or download address obtained after parsing */
+	unsigned int
+		app_version; /* The software version number obtained after parsing */
 };
 
 /************************************************************
@@ -55,7 +62,8 @@ struct bin_header_info {
 *
 ************************************************************/
 struct bin_container {
-	unsigned int len; /* The size of the bin file obtained from the firmware */
+	unsigned int
+		len; /* The size of the bin file obtained from the firmware */
 	unsigned char data[]; /* Store the bin file obtained from the firmware */
 };
 
@@ -64,10 +72,12 @@ struct aw_bin {
 	unsigned int all_bin_parse_num; /* The number of all bin files */
 	unsigned int multi_bin_parse_num; /* The number of single bin files */
 	unsigned int single_bin_parse_num; /* The number of multiple bin files */
-	struct bin_header_info header_info[BIN_NUM_MAX]; /* Frame header information and other important data obtained after parsing */
-	struct bin_container info; /* Obtained bin file data that needs to be parsed */
+	struct bin_header_info header_info
+		[BIN_NUM_MAX]; /* Frame header information and other important data obtained after parsing */
+	struct bin_container
+		info; /* Obtained bin file data that needs to be parsed */
 };
 
-extern int aw_parsing_bin_file(struct aw_bin *bin);
+extern int aw9_parsing_bin_file(struct aw_bin *bin);
 int aw_parse_bin_header_1_0_0(struct aw_bin *bin);
 #endif
