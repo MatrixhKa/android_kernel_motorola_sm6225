c2978da286aae678e6381b3d3cf8fa2fec64878d
MatrixhKa <familiacomplicada1234@gmail.com>
2026-03-23 17:12:33 +0000

    kernel: sched: fair.c: Fix statement

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -173,9 +173,9 @@ static inline u64 scale_slice(u64 delta, struct sched_entity *se) {
 static inline struct task_struct *task_of(struct sched_entity *se);
 
 static void update_burst_score(struct sched_entity *se) {
-	if (!entity_is_task(se)) return;
   struct task_struct *p;
 
+	if (!entity_is_task(se)) return;
 	p = task_of(se);
 	u8 prio = p->static_prio - MAX_RT_PRIO;
 	u8 prev_prio = min(39, prio + se->burst_score);
